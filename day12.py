bf5156be47a50f5cdc8f37cad9d1140d4f10ce27
Lucas Joyal <lucasjoyal@gmail.com>
2023-12-12 21:42:11 -0500

    Slight improvement in performance for day 12
    Add early pruning for invalid branches.

--- day12.py
+++ day12.py
@@ -3,6 +3,7 @@
 from typing import List, Set, Tuple, Collection, Dict
 
 from dataclasses import dataclass
+from itertools import repeat
 
 @dataclass
 class DamagedRecord:
@@ -31,13 +32,31 @@ class DamagedRecord:
         """
         # Maybe too complex too soon
         pass
+        if self.is_complete():
+            # but really if we are checking for being invalid we should not be complete
+            return not self.is_valid()
+        # assume not complete, so a ? is remaining
 
+        # check if any groups are longer than the valid part of the record
+        valid_record = self.record[:self.record.find("?")]
+
+        valid_groups = list(filter(None, [r.strip(".") for r in valid_record.strip(".").split(".")]))
+
+        for vg, eg in zip(valid_groups, self.groups):
+            if len(vg) > eg:
+                return True
+
+        # Maybe valid
+        return False
 
 
 
 def possible_arrangements(r: DamagedRecord) -> int:
     if r.is_complete():
         return 1 if r.is_valid() else 0
+
+    if r.is_invalid():
+        return 0
 
     possible_records = []
     for state in [".", "#"]:
@@ -53,7 +72,9 @@ def get_possible_arrangement(r, state) -> DamagedRecord:
     assert not r.is_complete()
     return DamagedRecord(r.record.replace("?", state, 1), groups=r.groups)
 
-
+def apply_group(r) -> DamagedRecord:
+    next_group = r.groups[0]
+    # what next? no clue
 
 
 def parse(data) -> List[DamagedRecord]:
@@ -102,9 +123,42 @@ def day12(filename, expected=None):
 
     print(f"Result: {result}")
 
+def parse_part2(data) -> List[DamagedRecord]:
+    records = []
+    for line in data.split("\n"):
+        record, groups = line.split()
+        parsed_groups = [int(g) for g in groups.split(",")]
+        unfolded_groups = parsed_groups * 5
+        records.append(DamagedRecord("?".join([record for i in range(5)]), unfolded_groups))
+
+    return records
+
+def day12_part2(filename, expected=None):
+
+    with open(filename, "r") as f:
+        data = f.read().strip()
+
+    records = parse_part2(data)
+    print(records)
+    total_count = 0
+    for r in records:
+        count = possible_arrangements(r)
+        print(r, count)
+        total_count += count
+    result = total_count
+    if expected:
+        assert result == expected, f"expected {expected}, got {result}"
+
+
+
+    print(f"Result: {result}")
+
 if __name__ == "__main__":
     day12("day12_small.txt", expected=21)
     day12("day12.txt")
+    #day12_part2("day12_small.txt", expected=525152)
+    #day12_part2("day12.txt")
+
 
 # Start of problem:
 # This one is going to require some clever use of combinatorics.
@@ -122,3 +176,9 @@ if __name__ == "__main__":
 # It works! 23s so it could be faster, but it's simple enough
 #
 # Ouch, part 2 will need early pruning. And perhaps splitting of records?
+# Even with pruning, it won't work.
+#
+# combinatorics time?
+#
+# Smarter possible arrangements?
+# I think it's too late tonight.
